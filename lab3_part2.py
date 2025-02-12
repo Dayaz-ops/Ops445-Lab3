0b6acd7e6909a886d318a53a3f65ccdf8f160afe
Dayaz-ops <daniyalayaz@yahoo.ca>
2025-02-12 15:15:01 -0500

    Delete lab3_part2.py

--- lab3_part2.py
+++
@@ -1,8 +0,0 @@
-def return_text_value():
-    name = 'Terry'
-    greeting = 'Good Morning ' + name 
-    return greeting
-text = return_text_value()
-
-if "Morning" in text:
-    print("The function returned a morning greeting!")
